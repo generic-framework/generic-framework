ab51cf073314755248046e46d8657d370f3ec5b7
Alp Ersoy <ersoyyalp07@gmail.com>
2024-12-05 22:59:16 +0300

    Task Metodları Oluşturuldu. File metodları oluşturuldu

--- Main.Server.DataAccess/AppDbContext.cs
+++ Main.Server.DataAccess/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using Main.Server.Core.Entities;
 using Main.Server.Core.Entities.ProductEntities;
 using Main.Server.Core.Entities.RoleEntities;
+using Main.Server.Core.Entities.TaskEntities;
 using Main.Server.Core.Entities.UserEntities;
 using Microsoft.EntityFrameworkCore;
 namespace Main.Server.DataAccess
@@ -17,9 +19,12 @@ namespace Main.Server.DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<TaskEntity> TaskEntities { get; set; }
+        public DbSet<TaskFile> TaskFiles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
---
+++ Main.Server.Service/Services/AllServices/TaskService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Server.Core.Entities;
+using Main.Server.Core.Entities.TaskEntities;
+using Main.Server.Core.Repositories;
+using Main.Server.Core.Repositories.IRepositories;
+using Main.Server.Core.Services.IServices;
+using Main.Server.Core.UnitOfWorks;
+using Main.Server.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace Main.Server.Service.Services.AllServices
+{
+    public class TaskService : Service<TaskEntity>, ITaskService
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly AppDbContext _dbContext;
+
+        public TaskService(IGenericRepository<TaskEntity> repository, ITaskRepository taskRepository,
+            IUnitOfWorks unitOfWorks, AppDbContext dbContext) : base(repository, unitOfWorks)
+        {
+            _taskRepository = taskRepository;
+            _dbContext = dbContext;
+
+        }
+
+        public async Task<TaskFile> SaveFileAsync(string fileName, string filePath, long fileSize, int taskId)
+        {
+            var file = new TaskFile
+            {
+                FileName = fileName,
+                FilePath = filePath,
+                FileSize = fileSize,
+                TaskId = taskId,
+                CreatedDate = DateTime.UtcNow
+            };
+
+            _dbContext.TaskFiles.Add(file);
+            await _dbContext.SaveChangesAsync();
+            return file;
+        }
+    }
+}
