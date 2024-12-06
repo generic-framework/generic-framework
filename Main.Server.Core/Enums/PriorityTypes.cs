667ead24a38e7e0ea6501e49f1090a4c46f6586b
HAKAN\hemre <yesilhakanemre@gmail.com>
2024-12-07 01:04:27 +0300

    PriorityTypes enum eklendi

---
+++ Main.Server.Core/Enums/PriorityTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.Server.Core.Enums
+{
+    public enum PriorityTypes
+    {
+        Lowest = 1,
+        Log = 2,
+        Medium = 3,
+        Higest = 4,
+        High = 5
+
+    }
+}
