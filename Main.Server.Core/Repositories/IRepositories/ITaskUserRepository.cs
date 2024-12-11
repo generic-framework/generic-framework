4b32aaf75ff2499b759a689182894f4c8ca1c523
Alp Ersoy <ersoyyalp07@gmail.com>
2024-12-12 00:13:37 +0300

    TaskUser ile ilgili düzenlemeler yapıldı

---
+++ Main.Server.Core/Repositories/IRepositories/ITaskUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Server.Core.Entities.ProjectEntities;
+using Main.Server.Core.Entities.TaskEntities;
+
+namespace Main.Server.Core.Repositories.IRepositories
+{
+    public interface ITaskUserRepository : IGenericRepository<TaskUser>
+    {
+    }
+}
---
+++ Main.Server.DataAccess/Repositories/AllRepositories/TaskUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Server.Core.Entities.ProjectEntities;
+using Main.Server.Core.Entities.TaskEntities;
+using Main.Server.Core.Repositories.IRepositories;
+
+namespace Main.Server.DataAccess.Repositories.AllRepositories
+{
+    public class TaskUserRepository : GenericRepository<TaskUser>, ITaskUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskUserRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
